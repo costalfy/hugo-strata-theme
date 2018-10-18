29cbfbc7e389edeb49f0c398a10afec7688da503
Andy Costanza <costalfy@gmail.com>
2018-10-19 00:21:10 +0200

    add isotope

---
+++ static/js/load-isotope.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    var $grid = $('.grid').imagesLoaded( function() {
+        $grid.isotope({
+            itemSelector: '.grid-item',
+            layoutMode: 'fitRows',
+            percentPosition: true,
+            masonry: {
+                columnWidth: '.grid-sizer'
+            }
+        });
+      });
+});
